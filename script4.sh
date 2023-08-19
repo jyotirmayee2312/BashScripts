5fc75851ba75e69a35f476f20d468c5e59f5294f
Jyotirmayee Parida <81229488+jyotirmayee2312@users.noreply.github.com>
2023-08-19 09:23:37 +0530

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-# Specify the directory where the files are located
-directory="/path/to/directory"
+# Set the directory path where you want to delete files
+target_directory="/path/to/target/directory"
 
-# Specify the number of days to keep files
-days_to_keep=7
+# Calculate the timestamp for files older than 1 minute
+timestamp=$(date -d "-1 minute" +%s)
 
-# Specify the file extensions or patterns for temporary files and logs
-file_extensions=("*.tmp" "*.log")
-
-# Calculate the cutoff date
-cutoff_date=$(date -d "$days_to_keep days ago" +%Y-%m-%d)
-
-# Delete files older than the cutoff date and matching the specified extensions/patterns
-for extension in "${file_extensions[@]}"; do
-  find "$directory" -type f -name "$extension" ! -newermt "$cutoff_date" -delete
-done
-
-# Print a message indicating the files that have been deleted
-echo "Deleted temporary files and logs older than $cutoff_date in $directory"
-
+# Find and delete files older than the specified timestamp
+find "$target_directory" -type f -mmin +1 -exec rm -f {} \;
